0892401f8767a576d1e5b52eab3a3f7913b519f6
Prince-Dest1ny <100820991+Prince-Dest1ny@users.noreply.github.com>
2024-03-26 13:04:57 +0800

    Added recipes for:     Bundle     miner's cooking guide

--- server_scripts/recipes.js
+++ server_scripts/recipes.js
@@ -1,6 +1,19 @@
 //onEvent('recipes', event => {
-ServerEvents.recipes(event => {
-    event.shapeless('create_central_kitchen:miners_cooking_guide', [
-        'minecraft:book'
-        'minecraft:iron_ingot'
-])})
+    ServerEvents.recipes(event => {
+        event.shapeless('create_central_kitchen:miners_cooking_guide', [
+            'miners_delight:cave_carrot',
+            'farmersdelight:canvas',
+            'create:sturdy_sheet'
+            ])
+        event.shaped('minecraft:bundle',
+            [
+                'ABA',
+                'B B',
+                'BBB'
+            ],
+            {
+                A: 'minecraft:string',
+                B: 'minecraft:leather'
+            }
+            )
+        })
