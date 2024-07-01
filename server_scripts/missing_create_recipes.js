01b37d0e6f16fe3a6c1cb28c398686fc84362707
thebestharrison1221 <100962097+Thebestharrison1221@users.noreply.github.com>
2024-07-01 20:36:01 +0100

    fixed stuff and added unfired and unpolished steel

--- server_scripts/missing_create_recipes.js
+++ server_scripts/missing_create_recipes.js
@@ -13,23 +13,56 @@ ServerEvents.recipes(event => {
     event.stonecutting('4x design_decor:industrial_iron_boiler', '#create:industrial_iron_block')
     //bronze
 main
-    event.recipes.create.mixing('kubejs:bronze_ingot', '2x create:brass_ingot', 'create:zinc').heated()
-    event.recipes.create.mixing('kubejs:bronze_ingot', 'minecraft:copper', 'create:zinc').heated()
-    event.recipes.create.pressing(['kubejs:bronze_block', Item.of('kubejs:bronze_ingot').withChance(50)], '7x kubejs:bronze_ingot')
+
+    //making bronze
+    event.recipes.create.mixing('2x kubejs:bronze_ingot', '2x create:brass_ingot', 'create:zinc').heated()
+    //pressing bronze block back to ingots
+    event.recipes.create.pressing(['kubejs:bronze_block', Item.of('kubejs:bronze_ingot').withChance(50)], '8x kubejs:bronze_ingot')
+    //block from ingot
     event.shapeless(
-      Item.of('kubejs:bronze_ingot', 9), // arg 1: output
-      [
+      Item.of('kubejs:bronze_ingot', 9), [
         'kubejs:bronze_block',
-      ]
-    )
+      ])
+    //9 ingot to 1 block
     event.shaped('kubejs:bronze_block', [
     'AAA', 
     'AAA',
     'AAA'  
   ], {
     A: 'kubejs:bronze_ingot' 
-  }
-)
+  })
   //steel
 
+  //pressing steel block back to ingots
+   event.recipes.create.pressing(['kubejs:unpolished_steel_block', Item.of('kubejs:unpolished_steel_ingot').withChance(50)], '8x kubejs:unpolished_steel_ingot')
+  
+   //block from ingot
+   event.shaped('kubejs:unpolished_steel_block', [
+     'AAA', 
+     'AAA',
+     'AAA'  
+   ], {
+     A: 'kubejs:unpolished_steel_ingot' 
+   })
+
+   //polish unpolished block/item
+   ServerEvents.recipes(event => {
+    event.recipes.create.sandpaper_polishing('#createbigcannons:nethersteel_ingot', 'kubejs:unpolished_steel_block')
+   })
+   ServerEvents.recipes(event => {
+    event.recipes.create.sandpaper_polishing('#createbigcannons:nethersteel_ingot', 'kubejs:unpolished_steel_ingot')
+   })
+
+   //block from ingot
+   event.shaped('kubejs:unfired_steel_block', [
+     'AAA', 
+     'AAA',
+     'AAA'  
+    ], {
+     A: 'kubejs:unfired_steel_ingot' 
+   })
+   //9 ingot to 1 block
+   event.shapeless('9x unfired_steel_ingot', [
+     'unfired_steel_block',
+   ])
 })
--- startup_scripts/additem.js
+++ startup_scripts/additem.js
@@ -6,8 +6,13 @@ StartupEvents.registry('item', event => {
     //register new items here
     //event.create('example_item').displayName('Example Item')
     
-    event.create('steel_ingot').displayName('Steel Ingot')
+    //steel
+    event.create('unpolished_steel_ingot').displayName('Unpolished Steel Ingot')
+    event.create('unfired_steel_ingot').displayName('Unfired Steel Ingot')
+
+    //bronze
     event.create('bronze_ingot').displayName('Bronze Ingot')
+
 })
 
 StartupEvents.registry('block', event => {
@@ -15,11 +20,11 @@ StartupEvents.registry('block', event => {
     //event.create('example_block').material('wood').hardness(1.0).displayName('Example block)
  main
 
+    //steel
+    event.create('unpolished_steel_block').displayName('Unpolished Steel Block')
+    event.create('unfired_steel_block').displayName('Unfired Steel Block')
+
+    //bronze
     event.create('bronze_block').displayName('Bronze Block')
-    event.create('steel_block').displayName('Steel Block')
-    
-=======
-    event.create('bronze_block').displayName('Bronze Block')
-    event.create('steel_block').displayName('Steel Block')
 
 })
